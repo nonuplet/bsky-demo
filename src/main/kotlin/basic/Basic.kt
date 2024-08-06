5e682355cc9912e47bb7a9f152339994bcccfdaf
Kokonoe <130939038+nonuplet@users.noreply.github.com>
2024-08-06 11:54:41 +0900

    タイムラインのreasonからリポストか銅か判定する関数の実装

--- src/main/kotlin/basic/Basic.kt
+++ src/main/kotlin/basic/Basic.kt
@@ -8,6 +8,7 @@ import work.socialhub.kbsky.api.entity.com.atproto.server.ServerCreateSessionRes
 import work.socialhub.kbsky.domain.Service.BSKY_SOCIAL
 import work.socialhub.kbsky.model.app.bsky.feed.FeedDefsFeedViewPost
 import work.socialhub.kbsky.model.app.bsky.feed.FeedDefsPostView
+import work.socialhub.kbsky.model.app.bsky.feed.FeedDefsReasonRepost
 
 /**
  * ログイン
@@ -90,16 +91,26 @@ fun printPost(post: FeedDefsPostView) {
     val embeds = post.embed?.asImages?.images?.map { it.thumb }
 
     val text =
-"""***** --------------- *****
+        """***** --------------- *****
 [${post.author?.displayName} (${post.author?.handle})]
 ${post.record?.asFeedPost?.text}
 💬${post.replyCount}  🔁${post.repostCount}  💕${post.likeCount}
 EMBEDS:
-${embeds?.joinToString(separator = "\n") { it.toString() } ?: "" }
+${embeds?.joinToString(separator = "\n") { it.toString() } ?: ""}
 URI:${post.uri}
 CID:${post.cid}
 """
     println(text)
+}
+
+/**
+ * リポストかどうか
+ *
+ * @param reason
+ */
+fun printRepost(reason: FeedDefsReasonRepost?) {
+    if (reason == null) return
+    println("reposted by ${reason.by?.displayName} (${reason.by?.handle})")
 }
 
 fun basicSample() {
@@ -112,6 +123,7 @@ fun basicSample() {
     val timeline = getTimeline(accessJwt, 10)
     timeline.forEach {
         printPost(it.post)
+        printRepost(it.reason)
     }
 
     // 投稿
